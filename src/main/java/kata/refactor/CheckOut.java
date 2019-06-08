67c2c156df675b2c7b59e4ad28569f36d3b20895
llcain <llcain2012@gmail.com>
2019-06-08 10:13:28 -0500

    adding something

--- src/main/java/kata/refactor/CheckOut.java
+++ src/main/java/kata/refactor/CheckOut.java
@@ -22,36 +22,21 @@ public class CheckOut {
 
       System.out.println("\nThe character is: " + good);
 
-      switch (good) {
-         case 'A':
+   switch (good) {
+      case 'A':
+         totalSoFar = totalSoFar + 50;
+         break;
+      case 'B':
+         totalSoFar = totalSoFar + 30;
+         break;
+      case 'C':
+         totalSoFar = totalSoFar  + 20;
+         break;
+      case 'D':
+         totalSoFar = totalSoFar + 15;
+         break;
+   }
 
-            myACounter = myACounter + 1;
-
-            if (myACounter == 3) {
-               totalSoFar = totalSoFar + 30;
-               myACounter = 0;
-            }else {
-               totalSoFar =totalSoFar + 50;
-            }
-
-            break;
-         case 'B':
-
-            myBCounter = myBCounter + 1;
-
-            if (myBCounter == 2) {
-               totalSoFar = totalSoFar + 15;
-            }else {
-               totalSoFar = totalSoFar + 30;
-            }
-            break;
-         case 'C':
-            totalSoFar = totalSoFar + 20;
-            break;
-         case 'D':
-            totalSoFar = totalSoFar + 15;
-            break;
-      }
  //hi
        //later
 
