d76854a46efa36c34c606a3e661d61939698aa4f
llcain <llcain2012@gmail.com>
2019-06-12 01:44:07 -0500

    final test

--- src/main/java/kata/refactor/CheckOut.java
+++ src/main/java/kata/refactor/CheckOut.java
@@ -35,7 +35,14 @@ public class CheckOut {
          }
          break;
       case 'B':
-           totalSoFar = totalSoFar + 30;
+
+         myBCounter = myBCounter + 1;
+
+         if (myBCounter == 2) {
+            totalSoFar = totalSoFar + 15;
+         }else {
+            totalSoFar = totalSoFar + 30;
+         }
          break;
       case 'C':
          totalSoFar = totalSoFar  + 20;
